e9463904276d48c8b86d495e3c19a5a4141cbd8c
Gayathri13052005 <gayathrirao.be@gmail.com>
2025-03-01 20:36:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// const BASE_URL = "http://localhost:5000/api/auth";
+
+function toggleForms() {
+    document.getElementById("signup-form").classList.toggle("hidden");
+    document.getElementById("signin-form").classList.toggle("hidden");
+}
